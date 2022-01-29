f705740dd4f0fa233382165c0a4221bb37f46ded
MICAH <micah.ahn@pcc.edu>
2022-01-29 11:12:58 -0800

    added email sending function. unsure how to get the temp and TDS values to display in html.

--- fishTank/fishTank.ino
+++ fishTank/fishTank.ino
@@ -32,6 +32,17 @@
 #define ONE_HOUR            60*60*1000L	    // 1 Hour
 #define LOOP_UPDATE_DELAY   5*1000L         // 5 Seconds
 
+#define SMTP_HOST "smtp.gmail.com"                      // Mail Server name
+#define SMTP_PORT 465                                   // Use Port 465 for Gmail
+#define AUTHOR_EMAIL "PSU.FishTankMonitor@gmail.com"    // Email sender credentials
+#define AUTHOR_PASSWORD "PortlandState"                 // Sending email password
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "enter email here"
+
+/* The SMTP Session object used for Email sending */
+SMTPSession smtp;
+
 // * Starting Variables
 // loop update variable
 unsigned long loopTime = 0L ;
@@ -516,4 +527,103 @@ int getMedianNum(int bArray[], int iFilterLen)
 		bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
 
 	return bTemp;
+}
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
+/* Function to send email to user */ 
+void SendEmail(int tdsValue, int temperature){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Connecting to AP");
+  WiFi.begin(ssid, pwd);
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /** Enable the debug via Serial port
+   * none debug or 0
+   * basic debug or 1
+  */
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "Fish Tank Status";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "Update";
+  // message.addRecipient("Sara", RECIPIENT_EMAIL);
+
+  /*Send HTML message*/
+  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Enter text here</h1><p>- Sent from Fish Tank Monitor</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  /* Set the custom message header */
+  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
+
+  // Disconnect WiFi
+   WiFi.disconnect(true);
+   WiFi.mode(WIFI_OFF);
+}
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
 }
